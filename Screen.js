089184179108f1106ebd6fb1cfd0be9c1274cd8a
harry lachenmayer <harrylachenmayer@gmail.com>
2021-11-17 17:26:16 +0000

    minimal stack navigation bug repro

--- Screen.js
+++ Screen.js
@@ -1,44 +1,45 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from 'react-native-reanimated';
-import {View, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
+import {Button, Text, View} from 'react-native';
+import Reanimated, {FadeIn} from 'react-native-reanimated';
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+const Stack = createStackNavigator();
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
-
+export default function ScreensBugRepro() {
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-      }}>
-      <Animated.View
-        style={[
-          {width: 100, height: 80, backgroundColor: 'black', margin: 30},
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="First" component={FirstScreen} />
+        <Stack.Screen name="Second" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function FirstScreen({navigation}) {
+  return (
+    <View>
+      <Button onPress={() => navigation.navigate('Second')} title="click me" />
+    </View>
+  );
+}
+
+const longFade = FadeIn.duration(10000);
+
+function SecondScreen() {
+  return (
+    <View>
+      <Text>
+        go back from here. you won't be able to press the "click me" button
+        again until the exiting animation is finished (ie. 10 seconds).
+      </Text>
+      <Reanimated.View
+        style={{backgroundColor: 'red'}}
+        entering={longFade}
+        exiting={longFade}>
+        <Text>slooooow layout animation :)</Text>
+      </Reanimated.View>
     </View>
   );
 }
